ead3356b3a0784f85066c1a2d4004cdc728a0dd7
lubieowoce <lolzatu2@gmail.com>
2023-11-01 15:16:42 +0100

    fix FnPath type decl

--- packages/core/src/build/babel-types.d.ts
+++ packages/core/src/build/babel-types.d.ts
@@ -1,6 +1,7 @@
+import { NodePath } from "@babel/core";
 import * as t from "@babel/types";
 
 export type FnPath =
-  | t.NodePath<t.ArrowFunctionExpression>
-  | t.NodePath<t.FunctionDeclaration>
-  | t.NodePath<t.FunctionExpression>;
+  | NodePath<t.ArrowFunctionExpression>
+  | NodePath<t.FunctionDeclaration>
+  | NodePath<t.FunctionExpression>;
